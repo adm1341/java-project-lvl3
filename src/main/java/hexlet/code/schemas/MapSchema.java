54dafa205bd5560dcb7293e11456f84b981a6329
adm134 <adm134@yandex.ru>
2021-11-17 12:26:48 +0300

    Переделка проверок MapSchema на предикаты.

--- src/main/java/hexlet/code/schemas/MapSchema.java
+++ src/main/java/hexlet/code/schemas/MapSchema.java
@@ -1,71 +1,44 @@
 package hexlet.code.schemas;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
+import java.util.function.Predicate;
 
 public final class MapSchema extends BaseSchema {
-    private int sizeof = -1;
-    private Map<String, BaseSchema> shapeSchem = new HashMap<>();
-
-    @Override
-    public boolean isValid(Object objIn) {
-
-        return checkRequired(objIn) && checkSizeof(objIn) && checkShape(objIn);
-    }
 
     @Override
     public MapSchema required() {
-        setRequired(true);
+        Predicate<Object> isNull = Objects::isNull;
+        Predicate<Object> isNotMap = x -> !(x instanceof Map);
+        addPredicateList(isNull);
+        addPredicateList(isNotMap);
         return this;
     }
 
-    private boolean checkRequired(Object objIn) {
-        if (this.isRequired()) {
-            return checkNullAndType(objIn);
-        }
-        return true;
-    }
-
-    private boolean checkShape(Object objIn) {
-        return shapeSchem.size() <= 0 || !checkNullAndType(objIn) || checkMapNested(objIn);
-    }
-
-    private boolean checkMapNested(Object objIn) {
-        Map<String, Object> mapIn = (Map<String, Object>) objIn;
-        for (Map.Entry<String, Object> entry : mapIn.entrySet()) {
-            if (shapeSchem.containsKey(entry.getKey())) {
-                BaseSchema schemaCheck = shapeSchem.get(entry.getKey());
-                if (!schemaCheck.isValid(entry.getValue())) {
-                    return false;
-                }
-            } else {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private boolean checkSizeof(Object objIn) {
-        if (sizeof != -1) {
-            if (checkNullAndType(objIn)) {
-                Map<Object, Object> mapIn = (Map<Object, Object>) objIn;
-                return mapIn.size() >= sizeof;
-            }
-        }
-        return true;
-    }
-
-    private boolean checkNullAndType(Object objIn) {
-        return !this.isNull(objIn) && objIn instanceof Map;
-    }
-
     public MapSchema sizeof(int intIn) {
-        sizeof = intIn;
+        Predicate<Object> isNull = Objects::isNull;
+        Predicate<Object> isNotMap = x -> !(x instanceof Map);
+
+        Predicate<Object> isNotSizeof = x -> !(((Map<Object, Object>) x).size() >= intIn);
+        addPredicateList(isNull);
+        addPredicateList(isNotMap);
+        addPredicateList(isNotSizeof);
         return this;
     }
 
     public MapSchema shape(Map<String, BaseSchema> shapeSchemIn) {
-        shapeSchem = shapeSchemIn;
+        Predicate<Object> isNull = Objects::isNull;
+        Predicate<Object> isNotMap = x -> !(x instanceof Map);
+
+        Predicate<Object> predicate = x ->
+                ((Map<Object, Object>) x).entrySet()
+                        .stream()
+                        .filter(z -> !shapeSchemIn.get(z.getKey()).isValid(z.getValue())).count() > 0;
+
+        addPredicateList(isNull);
+        addPredicateList(isNotMap);
+        addPredicateList(predicate);
+
         return this;
     }
 }
